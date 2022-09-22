ad9b59a532060b50010ffc3a85cae188e9a248b0
Angel Legorreta <legosurio@hotmail.com>
2022-09-22 11:36:29 -0500

    primeros pasos para entregar bien todo

---
+++ Ejercicios Infernales 2/Assets/Code/AllFixedCodes/BehaviorsSystem.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorsSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DistanceCalculation()
+    {
+        
+    }
+
+    void Seek()
+    {
+        
+    }
+
+    void Flee()
+    {
+        
+    }
+
+    void Arrival()
+    {
+        
+    }
+
+    void Wander()
+    {
+        
+    }
+
+    void Pursuit()
+    {
+        
+    }
+
+    void Evade()
+    {
+        
+    }
+}
