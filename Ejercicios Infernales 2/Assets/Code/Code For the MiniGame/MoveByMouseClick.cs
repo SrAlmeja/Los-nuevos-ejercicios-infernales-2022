9fa3041bbc4be15ecc0df1029d6d50ae61944dfd
Angel Legorreta <legosurio@hotmail.com>
2022-09-26 11:42:58 -0500

    Movimiento vuelve a servir

--- Ejercicios Infernales 2/Assets/Code/Code For the MiniGame/MoveByMouseClick.cs
+++ Ejercicios Infernales 2/Assets/Code/Code For the MiniGame/MoveByMouseClick.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class MoveByMouseClick : MonoBehaviour
 {
+    public GameObject targetToMove;
+    public Camera cam;
+    void Start()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("Tocar");
             RaycastHit hit;
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
-                transform.position = new Vector3(hit.point.x, 0, hit.point.z);
+                targetToMove.transform.position = new Vector3(hit.point.x, 0, hit.point.z);
+                Debug.Log("di clic en " + hit.point);
             }
         }
-        
+
     }
 }
