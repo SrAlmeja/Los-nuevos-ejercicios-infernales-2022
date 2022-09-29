3d7ba8a11098acfa649757af30d715e7c91bb031
Inquisidor <cdmx1895@amerike.edu.mx>
2022-09-29 14:54:00 -0500

    hoy

--- Ejercicios Infernales 2/Assets/Code/Grid/FindPositionMouse.cs
+++ Ejercicios Infernales 2/Assets/Code/Grid/FindPositionMouse.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class FindPositionMouse : MonoBehaviour
 {
+    Renderer objectRender;
+    [SerializeField] Material normalMat, redMat, floodMat;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectRender = GetComponent<Renderer> ();
+        objectRender.enabled = true;
+        objectRender.sharedMaterial = normalMat;
     }
 
     // Update is called once per frame
@@ -15,11 +20,22 @@ public class FindPositionMouse : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("Se ha usado el mouse");
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
-               Debug.Log(gameObject.name);
+                objectRender.sharedMaterial = floodMat;
             }
         }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Se ha usado el mouse");
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                objectRender.sharedMaterial = redMat;
+            }
+        }
+        
     }
 }
--- Ejercicios Infernales 2/Assets/Code/Grid/GridGenerator.cs
+++ Ejercicios Infernales 2/Assets/Code/Grid/GridGenerator.cs
@@ -7,22 +7,20 @@ public class GridGenerator : MonoBehaviour
     [SerializeField] private int height, width;
     [SerializeField] GameObject panels;
     GameObject piso;
-    private float x = 0, y = 0;
-    
-    
-    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i <height; i++)
+        int i, j;
+        
+        for (i = 0; i <height; i++)
         {
-            piso = Instantiate(panels, new Vector3(i+(panels.transform.position.x/3 + 1),0,0), transform.rotation);
-            for (int j = 0; j < width; j++)
+            for (j = 0; j < width; j++)
             {
+                piso = Instantiate(panels, new Vector3(i+(panels.transform.position.x + 0.5f),0 ,j+(panels.transform.position.z + 0.5f)), transform.rotation);
                 piso.name = $"{i}-{j}";
-                
-                piso = Instantiate(panels, new Vector3(i+(panels.transform.position.x/3 + 1),0 ,j+(panels.transform.position.z/2 + 1)), transform.rotation);
-                piso.name = j.ToString();
             }
         }
     }
